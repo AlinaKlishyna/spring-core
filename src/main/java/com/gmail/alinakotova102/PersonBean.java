5500f62d561181cd075f30634ddd5afd2691beee
Alina Klishyna <alinakotova102@gmail.com>
2024-07-22 13:41:01 +0200

    creating first bean

---
+++ src/main/java/com/gmail/alinakotova102/PersonBean.java
@@ -0,0 +1,37 @@
+package com.gmail.alinakotova102;
+
+public class PersonBean {
+    private String name;
+    private String lastname;
+    private int age;
+
+    public PersonBean(String name, String lastname, int age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
